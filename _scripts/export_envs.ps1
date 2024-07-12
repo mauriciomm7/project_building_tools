6a5a30411f376707a2e988bbf48be88fd8886223
Mauricio M. M. <MauricioMM7@outlook.com>
2024-07-12 07:15:06 +0200

    updated message

--- _scripts/export_envs.ps1
+++ _scripts/export_envs.ps1
@@ -15,32 +15,32 @@ foreach ($env in $envDetails) {
     $yamlFileName = "$envName-environment.yml"
 
     # Activate the conda environment
-    Write-Output "Activating Conda environment: $envName"
+    Write-Host "Activating Conda environment: $envName"
     conda activate $envName
 
     # Export the conda environment directly to the Git repository path
-    Write-Output "Exporting Conda environment to $gitRepoPath\$yamlFileName"
+    Write-Host "Exporting Conda environment to $gitRepoPath\$yamlFileName"
     conda env export > "$gitRepoPath\$yamlFileName"
 }
 
 # Change to the Git repository directory
-Write-Output "Changing to Git repository directory: $gitRepoPath"
+Write-Host "Changing to Git repository directory: $gitRepoPath"
 Set-Location -Path $gitRepoPath
 
 # Add the changes to Git
-Write-Output "Adding changes to Git repository..."
+Write-Host "Adding changes to Git repository..."
 git add *.yml
 
 # Commit the changes
-Write-Output "Committing changes to Git repository..."
+Write-Host "Committing changes to Git repository..."
 git commit -m $commitMessage
 
 # Push changes to the remote repository
-Write-Output "Pushing changes to remote Git repository..."
+Write-Host "Pushing changes to remote Git repository..."
 git push
 
 # Deactivate conda environment
-Write-Output "Deactivating Conda environment..."
+Write-Host "Deactivating Conda environment..."
 conda deactivate
 
-Write-Output "Script execution completed."
+Write-Host "Script execution completed."
